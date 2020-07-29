4da8596050802c0d97a4966891265266d2b51624
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-28 20:59:18 -0600

    data from the api is working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
-const Home = () => (
-  <h1>Home</h1>
-)
+
+const Home = () => {
+  const [starWars, setStarWars] = useState([])
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+      console.log(res.data)
+    })
+  })
+
+  // <h1>Home</h1>
+  return (
+    <div>
+    <h1>Home</h1>
+    </div>
+  )
+}
 
 export default Home
