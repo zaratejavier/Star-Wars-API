6ff0ab899ec9945482fe7d2e4cfaafedfe8b6361
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-29 13:14:12 -0600

    information from different api is now working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,42 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
+import { Card } from "semantic-ui-react"
 
 
 const Home = () => {
   const [starWars, setStarWars] = useState([])
 
+
   useEffect(() => {
-    axios.get('https://swapi.dev/api/people')
+    getStarWarsData();
+  }, [])
+  
+  const getStarWarsData = ()=> {
+    // axios.get('https://swapi.dev/api/people')
+    axios.get('https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json')
       .then(res => {
         console.log(res.data)
-        setStarWars(res.data.results)
+        setStarWars(res.data)
+        console.log(res.data)
     })
-  },[])
+  }
 
   const renderNames = () => {
     return starWars.map(star => (
-      <h1>{star.name}</h1>
+      // <Card>
+      //   <Card.Header>{star}</Card.Header>
+      // </Card>
+      <div>
+        {star.name}
+        Length: {star.length}
+
+      </div>
     ))
   }
 
   return (
     <div>
+      <h3>Star Wars</h3>
       {renderNames()}
     </div>
   )
