b4d6ac753811df50e9be94314b75e910b30b772f
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-28 20:16:44 -0600

    about component is now working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Home from "./components/Home"
 import { Route } from 'react-router-dom';
+import Home from "./components/Home"
+import About from "./components/About"
 
 const App = () => (
-  <Route exact path="/" component={Home} />
-  
+  <div>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About}/>
+  </div>
 )
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Home = () => (
-  <h1>Helle World</h1>
+  <h1>Home</h1>
 )
 
 export default Home
