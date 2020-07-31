e490c5f203928b3cc0bbdc0b5167009b66e0cca0
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-30 23:30:30 -0600

    planets are displaying on the web page

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,8 +1,33 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import Axios from "axios"
 
 const Planets = () => {
+  const [planets, setPlanets] = useState([])
+
+  useEffect(() => {
+    getPlanets()
+  }, [])
+
+  const getPlanets = () => {
+    Axios.get('https://swapi.dev/api/planets/')
+      .then(res => {
+        console.log(res.data)
+        setPlanets(res.data.results)
+
+      })
+  }
+
+  const renderPlanets = () => {
+    return planets.map(planet => (
+      <p>{planet.name}</p>
+    ))
+  }
+
   return (
-    <h3>Planets</h3>
+    <>
+      <h1>Planets</h1>
+      {renderPlanets()}
+    </>
   )
 }
 
