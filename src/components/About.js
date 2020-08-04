8c6a7d756f7d642c4f2ca864f0ca93fe15469f6f
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-04 09:39:23 -0600

    added a sentence on the about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,10 @@
 import React from "react"
 
 const About = () => (
-  <h1>About</h1>
+  <div>
+    <h1>About</h1>
+    <p>This website is is taking information from the Stars Wars API and displaying it on the screen</p>
+  </div>
 
 )
 
