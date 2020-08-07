66d8f43e35e66dacc7da3cc697f8e6cbdc376daf
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-07 08:48:59 -0600

    Vehicle ddata is now being logged

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import Axios from "axios"
 
 const Vehicles = () => {
+  const [Vehicles, setVehicles] = useState([])
+
+  useEffect(() => {
+
+  }, [])
+  
+  const getVehicles = () => {
+    Axios.get('https://swapi.dev/api/vehicles')
+      .then(res => {
+        debugger
+      console.log(res.data)
+    })
+  }
 
   return(
   <div>
-    <h1>This is the Vehicles page</h1>
+      <h1>This is the Vehicles page</h1>
+      {getVehicles()}
   </div >
   )
 }
